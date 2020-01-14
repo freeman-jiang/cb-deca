b842a175d3379573bd94c7d22ccced0d0b17f575
freeman-jiang <56516912+freeman-jiang@users.noreply.github.com>
2020-01-14 10:08:05 -0500

    lol

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,14 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
+var btn = $('#backtotop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
 });
